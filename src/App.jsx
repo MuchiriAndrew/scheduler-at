19c6c98292220225d597a865d9b302d4a226a31e
MylesMburu <ngicumyles@gmail.com>
2023-09-27 21:38:52 +0300

    schedule

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import TaskAdder from './components/TaskAdder'
 import Home from './components/pages/Home'
+import ScheduleDisplay from './components/pages/ScheduleDisplay'
 import Tasks from './components/pages/Tasks'
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path='/classes' element = {<Home/>}/>
         <Route path='/tasks' element = {<Tasks/>}/>
+        <Route path='/schedule' element = {<ScheduleDisplay/>}/>
       </Routes>
     </Router>
     </>
