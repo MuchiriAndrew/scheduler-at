68433675163a39e8e58caa8f40e5db215502ec76
MuchiriAndrew <kariukia225@gmail.com>
2023-09-27 13:11:15 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import ClassAdder from './components/ClassAdder'
+// import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+
+
+function App() {
+  return (
+    <>
+    <ClassAdder />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/ClassAdder.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "../styles/ClassAdder.css"
+
+function ClassAdder() {
+  return (
+    <>
+        <h1>Hello World</h1>
+    </>
+  )
+}
+
+export default ClassAdder
